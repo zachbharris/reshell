b0201ba0f02208f1cac181a67c3e8f11764706bb
Zach Harris <zachbharris94@gmail.com>
2024-03-25 23:45:49 -0500

    start adding devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-import Device from "@/components/Devices";
+'use client'
+
 import Sidebar from "@/components/Sidebar";
 import { cn } from "@/lib/utils";
+import * as DMG from "@/components/Devices/GameboyOriginal"
 
 export default function Home() {
   return (
@@ -12,8 +14,16 @@ export default function Home() {
           "dark:bg-[radial-gradient(#171717_1px,transparent_1px)] [background-size:16px_16px]",
         )}
       >
-        <Device side="front" />
-        <Device side="back" />
+        <DMG.FrontShell>
+          <DMG.ScreenProtector x="20" y="41" />
+          <DMG.Screen x="65" y="63" />
+          <DMG.Button x="228" y="287" />
+          <DMG.Button x="181" y="308" />
+          <DMG.DPad x="22.5" y="287" />
+          <DMG.MenuButton x="85" y="375" />
+          <DMG.MenuButton x="134" y="375" />
+          <DMG.SpeakerVent x="177" y="370" />
+        </DMG.FrontShell>
       </div>
       <Sidebar />
     </main>
